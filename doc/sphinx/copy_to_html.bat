cd42303d8d155531252876a34f886e99017924dd
Thomas Cools <thomas.cools@onsemi.com>
2021-03-16 08:48:23 +0100

    Update copy doc bat

--- doc/sphinx/copy_to_html.bat
+++ doc/sphinx/copy_to_html.bat
@@ -1,13 +1,14 @@
 set doc_root=..
 
-set doc_html=%doc_root%\sphinx\build\html
+set doc_src_html=%doc_root%\sphinx\build\html
+set doc_tgt_html=%doc_root%\..\..\benchy-doc\docs
 
 set pwd=%CD%
-echo %doc_source%
 
-xcopy /y %doc_html%\* %doc_root%\html\*
-xcopy /y %doc_html%\_images\* %doc_root%\html\_images\*
-xcopy /y %doc_html%\_sources\* %doc_root%\html\_sources\*
-xcopy /y %doc_html%\_static\* %doc_root%\html\_static\*
+xcopy /y %doc_src_html%\* %doc_tgt_html%\*
+xcopy /y %doc_src_html%\_images\* %doc_tgt_html%\_images\*
+xcopy /y %doc_src_html%\_sources\* %doc_tgt_html%\_sources\*
+xcopy /y %doc_src_html%\_static\* %doc_tgt_html%\_static\*
 
 cd %pwd%
+pause
