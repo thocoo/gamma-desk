c1cd27b826cf2fa15a48fdbdd63414cecd81fa0e
Thomas Cools <thomas.cools@onsemi.com>
2021-03-17 16:41:08 +0100

    add test_colors

--- gdesk/test/__init__.py
+++ gdesk/test/__init__.py
@@ -22,6 +22,7 @@ class GammaDeskSuite(unittest.TestCase):
         gui.img.cmap('turbo')
         
     def test_code_2(self):
+        from gdesk import gui
         from pylab import plt
         
         plt.plot(gui.vs.mean(1))
@@ -39,11 +40,24 @@ class GammaDeskSuite(unittest.TestCase):
         plt.close('all')
         gui.menu_trigger('image', GammaDeskSuite.panid, ['Edit', 'Show Prior Image'])
         
+    def test_colors(self):
+        banner = r" _____                                      ______             _    " + "\n" \
+                 r"|  __ \                                     |  _  \           | |   " + "\n" \
+                 r"| |  \/  __ _  _ __ ___   _ __ ___    __ _  | | | |  ___  ___ | | __" + "\n" \
+                 r"| | __  / _` || '_ ` _ \ | '_ ` _ \  / _` | | | | | / _ \/ __|| |/ /" + "\n" \
+                 r"| |_\ \| (_| || | | | | || | | | | || (_| | | |/ / |  __/\__ \|   < " + "\n" \
+                 r" \____/ \__,_||_| |_| |_||_| |_| |_| \__,_| |___/   \___||___/|_|\_" + "\\"
+
+        fgs = [227, 227, 222, 217, 212, 207]
+        for fg, line in zip(fgs, banner.splitlines()):
+            bg = 236
+            pline = '\033[1m\033[48;5;{0:03d}m\033[38;5;{1:03d}m'.format(bg, fg) + line + '\033[0m'
+            print(pline)
+        
 def suite():
     suite = unittest.TestSuite()
+    suite.addTest(GammaDeskSuite('test_colors'))
     suite.addTest(GammaDeskSuite('test_code_1'))
-    suite.addTest(GammaDeskSuite('test_code_2'))
+    suite.addTest(GammaDeskSuite('test_code_2'))    
     return suite
 
-runner = unittest.TextTestRunner()
-runner.run(suite())        
