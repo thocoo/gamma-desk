bc7bd1bc5be34301a45dcc20aeb2709a43ef16f9
Thomas Cools <thomas.cools@onsemi.com>
2021-03-18 09:22:51 +0100

    Start and run test suite

---
+++ test/setup/test_gdesk.py
@@ -0,0 +1,16 @@
+from gdesk import gui, config, __version__
+
+gui.qapp.panels.ezm.set_perspective(config['layout']['image, levels & console'])
+
+panid = 1
+panel = gui.qapp.panels['console'][panid]
+panel.stdio.stdOutputPanel.addText('This is the start-up banner, before thread is started\n')
+
+test_code_1 = '''\
+import unittest
+from gdesk import test
+runner = unittest.TextTestRunner()
+runner.run(test.suite())        
+'''
+
+gui.console.execute_code(test_code_1, panid)
