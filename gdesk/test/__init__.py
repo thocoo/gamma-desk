af1913b07562db726e3f894890a999e90efff194
Thomas Cools <thomas.cools@onsemi.com>
2021-03-20 09:12:06 +0100

    Add extra unit tests

--- gdesk/test/__init__.py
+++ gdesk/test/__init__.py
@@ -7,7 +7,9 @@ class GammaDeskSuite(unittest.TestCase):
         from gdesk import gui
         from pylab import plt
         from pathlib import Path
-        
+
+        gui.load_layout('console')
+
         samplePath = Path(r'./samples')
 
         gui.img.select(1)
@@ -31,14 +33,29 @@ class GammaDeskSuite(unittest.TestCase):
         plt.grid()
         plt.show()
 
+
+    def test_small_loop_and_print(self):
+        from gdesk import gui
+        import time
+        
+        gui.clc()
+        
+        expectedOutput = ''
+
+        for i in range(42):
+            line = f'i = {i}'
+            print(line)       
+            expectedOutput += f'{line}\n'
+            time.sleep(0.01)
+            
+        text = gui.console.text()
+        assert expectedOutput == text
+
+
     def test_code_1(self):
         from gdesk import gui
         import numpy as np
-        import scipy.misc
-        
-        print('Hello World')
-        for i in range(10):
-            print(i)        
+        import scipy.misc        
         
         arr = scipy.misc.face()
         gui.show(arr)
@@ -46,9 +63,13 @@ class GammaDeskSuite(unittest.TestCase):
         gui.menu_trigger('image', GammaDeskSuite.panid, ['Canvas', 'Flip Horizontal'])
         gui.menu_trigger('image', GammaDeskSuite.panid, ['Canvas', 'Flip Vertical'])
         gui.menu_trigger('image', GammaDeskSuite.panid, ['Canvas', 'Rotate 180'])
+        
+        assert (arr == gui.vs).all()
+        
         gui.menu_trigger('image', GammaDeskSuite.panid, ['Image', 'to Monochroom'])
         gui.img.cmap('turbo')
-        
+
+
     def test_code_2(self):
         from gdesk import gui
         from pylab import plt
@@ -67,8 +88,16 @@ class GammaDeskSuite(unittest.TestCase):
 
         plt.close('all')
         gui.menu_trigger('image', GammaDeskSuite.panid, ['Edit', 'Show Prior Image'])
+
+
+    def test_colors(self):    
+        from gdesk import gui
+        from pylab import plt
         
-    def test_colors(self):
+        gui.clc()
+        plt.close('all')
+        gui.load_layout('console')
+        
         banner = r" _____                                      ______             _    " + "\n" \
                  r"|  __ \                                     |  _  \           | |   " + "\n" \
                  r"| |  \/  __ _  _ __ ___   _ __ ___    __ _  | | | |  ___  ___ | | __" + "\n" \
@@ -81,9 +110,15 @@ class GammaDeskSuite(unittest.TestCase):
             bg = 236
             pline = '\033[1m\033[48;5;{0:03d}m\033[38;5;{1:03d}m'.format(bg, fg) + line + '\033[0m'
             print(pline)
-        
+            
+        answer = gui.question('Does GammaDesk look nice in the console output?')
+        assert answer
+
+
 def suite():
     suite = unittest.TestSuite()
+    suite.addTest(GammaDeskSuite('test_screenstate_1'))
+    suite.addTest(GammaDeskSuite('test_small_loop_and_print'))
     suite.addTest(GammaDeskSuite('test_colors'))
     suite.addTest(GammaDeskSuite('test_code_1'))
     suite.addTest(GammaDeskSuite('test_code_2'))    
