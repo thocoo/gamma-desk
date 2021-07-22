b9021af1c1ed1a32e2f6ea7663d0b92267601b6d
Thomas Cools <cools.thomas@telenet.be>
2021-07-22 17:59:24 +0200

    Skip gdesk.dialogs.formlayout for Sphinx

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -14,13 +14,16 @@
 #
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('../../..'))
 
 print(sys.path)
 
+autodoc_mock_imports = ["gdesk.dialogs.formlayout"]
+
 from gdesk.core.conf import configure
 configure(matplotlib={'backend':'svg'})
-
+    
 # -- Project information -----------------------------------------------------
 
 project = 'gdesk'
