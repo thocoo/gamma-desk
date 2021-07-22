6d06f41d7b4ee903cfb439d13df332c45385137e
Thomas Cools <cools.thomas@telenet.be>
2021-07-22 17:27:24 +0200

    Remove hard paths

--- doc/sphinx/generate_doc.bat
+++ doc/sphinx/generate_doc.bat
@@ -1,6 +1,4 @@
-SET PYTHONPATH=%~dp0\..\..
-
-set sphinxdir=C:\tools\python\python38\scripts
+REM SET PYTHONPATH=%~dp0\..\..
 
 set doc_root=.
 set code_source=..\..\gdesk
@@ -11,8 +9,8 @@ set doc_html=%doc_root%\build\html
 set pwd=%CD%
 echo %doc_source%
 
-%sphinxdir%\sphinx-apidoc.exe --separate -f -o %doc_source% %code_source% > sphinx-apidoc.log 2>&1
-%sphinxdir%\sphinx-build.exe -E -b html -n %doc_source% %doc_html% > sphinx-build-html.log 2>&1
+sphinx-apidoc.exe --separate -f -o %doc_source% %code_source% > sphinx-apidoc.log 2>&1
+sphinx-build.exe -E -v -b html -n %doc_source% %doc_html% > sphinx-build-html.log 2>&1
 
 
 cd %pwd%
