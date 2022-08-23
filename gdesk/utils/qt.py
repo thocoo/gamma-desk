2dc29d84f9758932d6a56c6ddf0646879b299f0c
Thomas Cools <cools.thomas@telenet.be>
2022-08-23 19:06:01 +0200

    Also detect pyside if using pyside6

--- gdesk/utils/qt.py
+++ gdesk/utils/qt.py
@@ -3,10 +3,10 @@ try:
 except:
     PYSIDE, PYQT4 = False, False
     
-from qtpy import PYSIDE2, PYQT4, PYQT5
+from qtpy import PYSIDE2, PYQT4, PYQT5, PYSIDE6, PYQT6
     
 def using_pyside():
-    return PYSIDE or PYSIDE2
+    return PYSIDE or PYSIDE2 or PYSIDE6
     
 def using_pyqt():
-    return PYQT4 or PYQT5
+    return PYQT4 or PYQT5 or PYQT6
