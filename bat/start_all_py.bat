bcf83d511ac23265a9de468b65f6820a9fe303fe
Floris Lambrechts <floris.lambrechts@industrialise.be>
2025-02-19 14:39:52 +0100

    dev: Enable 'pyside2' and 'pyside6' extras
    Only on Python 3.10 and lower; from 3.11 PySide6 is the only option.

---
+++ bat/start_all_py.bat
@@ -0,0 +1,28 @@
+:: Start GDesk with a all supported Python versions.
+:: If the first argument is 'test' then execute a self-test procedure.
+:: Example:
+:: start_all_py.bat test
+@echo OFF
+
+set TEST=%1
+
+FOR %%G IN ("3.8"
+            "3.9"
+            "3.10"
+            "3.11"
+            "3.12"
+            "3.13"
+            ) DO (
+            IF /I "%PYTHON_VERSION%"=="%%~G" GOTO SUPPORTED
+)
+
+:SUPPORTED
+cd ..
+if /I "%TEST%" == "test" (
+    uv run --python %PYTHON_VERSION% --group pyside2 python -m gdesk -i .\test\setup\test_gdesk.py
+    uv run --python %PYTHON_VERSION% --group pyside6 python -m gdesk -i .\test\setup\test_gdesk.py
+) else (
+    uv run --python %PYTHON_VERSION% --group pyside2 python -m gdesk
+    uv run --python %PYTHON_VERSION% --group pyside6 python -m gdesk
+)
+GOTO :EOF
