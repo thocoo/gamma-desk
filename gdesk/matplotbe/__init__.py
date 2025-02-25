288f7e9e28c713d3c9f8886569ef9740e513efad
Floris Lambrechts <floris.lambrechts@industrialise.be>
2025-02-25 11:51:21 +0100

    fix(typo): Support pyplot up to v3.11

--- gdesk/matplotbe/__init__.py
+++ gdesk/matplotbe/__init__.py
@@ -27,7 +27,7 @@ from packaging import version
 from .. import gui
 
 
-if not version.parse('3.2') <= version.parse(matplotlib.__version__) < version.parse('3.10'):
+if not version.parse('3.2') <= version.parse(matplotlib.__version__) < version.parse('3.12'):
     warnings.warn(
         f'Matplotlib version {matplotlib.__version__} not supported.\n'
         f'Version should be 3.2 to 3.11'
