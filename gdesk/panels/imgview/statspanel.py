66de872e4e1abedf86b946b66df98711d694cba3
Thomas Cools <thomas.cools@ovt.com>
2025-05-23 14:49:21 +0200

    Used default widget spacings on VisibilityDialog

--- gdesk/panels/imgview/statspanel.py
+++ gdesk/panels/imgview/statspanel.py
@@ -527,8 +527,8 @@ class VisibilityDialog(QtWidgets.QDialog):
         self.setWindowTitle('Masks Visible')                        
                 
         self.vbox = QtWidgets.QVBoxLayout()
-        self.vbox.setContentsMargins(0,0,0,0)
-        self.vbox.setSpacing(0)
+        #self.vbox.setContentsMargins(0,0,0,0)
+        #self.vbox.setSpacing(0)
         self.setLayout(self.vbox)     
         
         self.toolbar = VisibilityToolBar(self)
@@ -548,7 +548,7 @@ class VisibilityDialog(QtWidgets.QDialog):
         self.table.customContextMenuRequested.connect(self.handleContextMenu)
         
         hbox = QtWidgets.QHBoxLayout()
-        hbox.setContentsMargins(10,10,10,10)
+        #hbox.setContentsMargins(10,10,10,10)
         self.vbox.addLayout(hbox)
         
         hbox.addStretch(1)
