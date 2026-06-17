378412956ddd4f78578c36b15a3e174e8d4c1113
Lou Vervecken <lvervecken@industrialise.be>
2026-06-17 10:39:42 +0200

    dev: Add support to select the ndim panel id in the proxy methods
    Default behavior is left untouched.

--- gdesk/panels/ndim/proxy.py
+++ gdesk/panels/ndim/proxy.py
@@ -9,6 +9,12 @@ from ...core.gui_proxy import GuiProxyBase, StaticGuiCall
 from ... import gui
 
 
+def _resolve_ndim_panel(panid=None):
+    if panid is not None:
+        return gui.qapp.panels['ndim'][panid]
+    return gui.qapp.panels.selected('ndim')
+
+
 class NdimGuiProxy(GuiProxyBase):
     category = 'ndim'
     opens_with = ['.h5', '.hdf5', '.he5', '.gif', '.npz', '.mov', '.mp4', '.nc', '.nc4']
@@ -30,9 +36,19 @@ class NdimGuiProxy(GuiProxyBase):
         return panel.panid
 
     @StaticGuiCall
-    def open(filepath, new=False):
-        """Open a file dialog and open ndim data"""
-        if new:
+    def select(panid):
+        """Make the ndim panel with the given id the active (selected) panel"""
+        gui.qapp.panels['ndim'][panid].select()
+
+    @StaticGuiCall
+    def open(filepath, panid=None, new=False):
+        """Open a file dialog and open ndim data
+
+        :param panid: panel id to use; uses the selected panel when None.
+        """
+        if panid is not None:
+            panel = gui.qapp.panels['ndim'][panid]
+        elif new:
             NdimGuiProxy.new()
             panel = gui.qapp.panels.selected('ndim')
         else:
@@ -44,7 +60,7 @@ class NdimGuiProxy(GuiProxyBase):
         return panel.panid
 
     @StaticGuiCall
-    def load_data(data, name=None, dim_names=None, dim_scales=None,  new=False) -> int:
+    def load_data(data, name=None, dim_names=None, dim_scales=None, panid=None, new=False) -> int:
         """Load a np ndarray or xarray DataArray into an ndim panel, open a new panel if needed
 
         The DataArray can contain additional annotation such as the names of the dimensions,
@@ -63,10 +79,13 @@ class NdimGuiProxy(GuiProxyBase):
         :param name: optional name of this data, is used when saving to hdf5.
         :param dim_names: optional list of length ndim with names per dimensions.
         :param dim_scales: optional list with (name, scale) tuples with scale values for each entry in a dim.
+        :param panid: panel id to use; uses the selected panel when None. Takes precedence over new.
         :param new: open a new panel if True, otherwise use the current panel if it exists.
         :return: the panel id of the opened panel.
         """
-        if new:
+        if panid is not None:
+            panel = gui.qapp.panels['ndim'][panid]
+        elif new:
             NdimGuiProxy.new()
             panel = gui.qapp.panels.selected('ndim')
         else:
@@ -78,49 +97,72 @@ class NdimGuiProxy(GuiProxyBase):
         return panel.panid
 
     @StaticGuiCall
-    def update_data(data):
+    def update_data(data, panid=None):
         """Update the data in the current panel leaving all the rest as is
 
         This can be used to update the data when the dimensions are the same, but the data has changed.
+
+        :param panid: panel id to use; uses the selected panel when None.
         """
-        panel = gui.qapp.panels.selected('ndim')
+        panel = _resolve_ndim_panel(panid)
         panel.update_data(data)
 
     @StaticGuiCall
-    def get_data() -> 'np.ndarray':
-        """Get the data from the current panel"""
-        panel = gui.qapp.panels.selected('ndim')
+    def get_data(panid=None) -> 'np.ndarray':
+        """Get the data from the current panel
+
+        :param panid: panel id to use; uses the selected panel when None.
+        """
+        panel = _resolve_ndim_panel(panid)
         return panel.main_widget.data
 
     @StaticGuiCall
-    def get_data_name() -> str:
-        """Get the name of the data from the current panel"""
-        panel = gui.qapp.panels.selected('ndim')
+    def get_data_name(panid=None) -> str:
+        """Get the name of the data from the current panel
+
+        :param panid: panel id to use; uses the selected panel when None.
+        """
+        panel = _resolve_ndim_panel(panid)
         return panel.main_widget.data_name
 
     @StaticGuiCall
-    def get_dim_names() -> List[str]:
-        """Get the names of the dimensions from the current panel"""
-        panel = gui.qapp.panels.selected('ndim')
+    def get_dim_names(panid=None) -> List[str]:
+        """Get the names of the dimensions from the current panel
+
+        :param panid: panel id to use; uses the selected panel when None.
+        """
+        panel = _resolve_ndim_panel(panid)
         return panel.main_widget.dim_names
 
     @StaticGuiCall
-    def get_dim_scales(): # -> List[Tuple[str | None, List | None]]
-        """Get the scales of the dimensions from the current panel"""
-        panel = gui.qapp.panels.selected('ndim')
+    def get_dim_scales(panid=None): # -> List[Tuple[str | None, List | None]]
+        """Get the scales of the dimensions from the current panel
+
+        :param panid: panel id to use; uses the selected panel when None.
+        """
+        panel = _resolve_ndim_panel(panid)
         return panel.main_widget.dim_scales
 
     @StaticGuiCall
-    def hide_row_column_color_selection():
-        panel = gui.qapp.panels.selected('ndim')
+    def hide_row_column_color_selection(panid=None):
+        """
+        :param panid: panel id to use; uses the selected panel when None.
+        """
+        panel = _resolve_ndim_panel(panid)
         return panel.main_widget.hide_row_column_color_selection()
 
     @StaticGuiCall
-    def show_row_column_color_selection():
-        panel = gui.qapp.panels.selected('ndim')
+    def show_row_column_color_selection(panid=None):
+        """
+        :param panid: panel id to use; uses the selected panel when None.
+        """
+        panel = _resolve_ndim_panel(panid)
         return panel.main_widget.show_row_column_color_selection()
 
     @StaticGuiCall
-    def clear_data():
-        panel = gui.qapp.panels.selected('ndim')
+    def clear_data(panid=None):
+        """
+        :param panid: panel id to use; uses the selected panel when None.
+        """
+        panel = _resolve_ndim_panel(panid)
         return panel.main_widget.clear_data()
