bd74458d8a25bea26d0e5003858936e58f31343c
Thomas Cools <thomas.cools@ovt.com>
2026-09-03 11:12:14 +0200

    Re-ordering histogram related stat funcs

--- gdesk/panels/imgview/imgdata.py
+++ gdesk/panels/imgview/imgdata.py
@@ -200,9 +200,7 @@ class OrderedStats(UserDict):
         else:        
             self.order.remove(key)
             self.order.insert(0, key)
-            
-    
-            
+                            
         
 class ImageStatistics(object):
 
@@ -426,138 +424,17 @@ class ImageStatistics(object):
                 return True
         else:        
             return False
-        
-        
-    def clear(self):
-        logger.debug(f'Clearing statistics cache for {self.name}')
-
-        if not self.bmask is None:
-            current_limits, current_steps = self.get_limits_and_steps()
-            if self.limits != current_limits or self.bmask.ndim != self.full_array.ndim:
-                self.mask_crop = None
-                self.bmask = None
-                self.mask_qimg = None                                
-
-        self._cache.clear()
-        
-        
-    def step_for_bins(self, bins):
-        if self.dtype in ['float16', 'float32', 'float64']:
-            return math.ceil(65536 / bins) 
             
-        if len(self._cache.keys()) == 0:
-            self.calc_histogram()
             
-        hist1 = self._cache['hist']  
-        
-        return math.ceil(len(hist1) / bins)
-    
-    
-    def isCleared(self):
-        return len(self._cache.keys()) == 0
-    
-        
-    def histogram(self, step=1):
-        if self.isCleared():
-            self.calc_histogram()
-        
-        hist1 = self._cache['hist']      
-            
-        if step > 1:
-            bins = len(hist1) // step
-            left = len(hist1) % step
-            tmp = hist1[:step*bins]            
-            if left > 0:
-                hist = np.r_[tmp.reshape(bins, step).sum(1), hist1[step*bins:].sum()]                
-            else:
-                hist = tmp.reshape(bins, step).sum(1)
-            return hist
-        else:
-            return hist1       
-        
-    def starts(self, step=1):
-        if len(self._cache.keys()) == 0:
-            self.calc_histogram()
-            
-        starts1 = self._cache['starts'] 
-
-        if step > 1:
-            return starts1[::step]
-        else:        
-            return starts1      
-    
-    def calc_histogram(self, bins=None, step=None):  
-        if isinstance(self.roi, np.ma.MaskedArray):
-            data = self.roi.compressed()
-        else:
-            data = self.roi
-            
-        if self.dtype in ['int8', 'uint8', 'int16', 'uint16']:
-            hist, starts, stepsize = fasthist.hist16bit(data, bins=None, step=1, use_numba=True)
-            
-        elif self.dtype in ['int32', 'uint32', 'int64', 'uint64', 'float16', 'float32', 'float64']:
-            hist, starts, stepsize = fasthist.histfloat(data, bins=65536, step=None, pow2snap=False, use_numba=True)
-            
-        self._cache['hist'] = hist
-        self._cache['starts'] = starts
-        self._cache['stepsize'] = stepsize            
-    
-    @property    
-    def bins(self):
-        return len(self.starts())
-        
-    def stepsize(self, step):
-        return self._cache['stepsize'] * step
-        
     def n(self):
         """Return the number of sample values to calculate statistics on."""
         if isinstance(self.roi, np.ma.MaskedArray):
             #return np.prod(self.roi.shape) - self.roi.mask.sum()
             return np.ma.count(self.roi)
         else:
-            return np.prod(self.roi.shape)
-        
-    def sum(self):
-        return (self.histogram() * self.starts()).sum()
-        
-    def mean(self):
-        n = self.n()
-        if n == 0:
-            return np.nan
-        return self.sum() / n
-
-    def sumsq(self):
-        return (self.histogram() * self.starts()**2).sum()
-        
-    def min(self):
-        hist = self.histogram()
-        starts = self._cache['starts']
-        if len(starts) > 0:
-            return starts[0]
-        else:
-            return np.nan
-        
-    def max(self):
-        hist = self.histogram()
-        starts = self._cache['starts']
-        if len(starts) > 0:
-            return starts[-1]
-        else:
-            return np.nan
-        
-    def std(self):
-        n = self.n()
-        
-        if n >= 2:
-            result = ((self.sumsq() - ((self.sum() * 1.0) ** 2) / n) / (n - 1))
-            if result >= 0:
-                return result ** 0.5
-            else:
-                return np.nan
-        else:
-            return np.nan
-        
-        
+            return np.prod(self.roi.shape)              
+            
+            
     def profile(self, axis=0):        
         roi = self.roi  
         array = self.full_array
@@ -580,6 +457,140 @@ class ImageStatistics(object):
             return x[not_masked_indices], y[not_masked_indices]
         else:                    
             return x, y
+        
+        
+    def clear(self):
+        logger.debug(f'Clearing statistics cache for {self.name}')
+
+        if not self.bmask is None:
+            current_limits, current_steps = self.get_limits_and_steps()
+            if self.limits != current_limits or self.bmask.ndim != self.full_array.ndim:
+                self.mask_crop = None
+                self.bmask = None
+                self.mask_qimg = None                                
+
+        self._cache.clear()                                                         
+                  
+
+    ##############################
+    # Histogram based statistics    
+    
+    def isCleared(self):
+        return len(self._cache.keys()) == 0  
+        
+        
+    def histogram(self, step=1):
+        if self.isCleared():
+            self.calc_histogram()
+        
+        hist1 = self._cache['hist']      
+            
+        if step > 1:
+            bins = len(hist1) // step
+            left = len(hist1) % step
+            tmp = hist1[:step*bins]            
+            if left > 0:
+                hist = np.r_[tmp.reshape(bins, step).sum(1), hist1[step*bins:].sum()]                
+            else:
+                hist = tmp.reshape(bins, step).sum(1)
+            return hist
+        else:
+            return hist1       
+            
+        
+    def starts(self, step=1):
+        if len(self._cache.keys()) == 0:
+            self.calc_histogram()
+            
+        starts1 = self._cache['starts'] 
+
+        if step > 1:
+            return starts1[::step]
+        else:        
+            return starts1      
+            
+    
+    def calc_histogram(self, bins=None, step=None):  
+        if isinstance(self.roi, np.ma.MaskedArray):
+            data = self.roi.compressed()
+        else:
+            data = self.roi
+            
+        if self.dtype in ['int8', 'uint8', 'int16', 'uint16']:
+            hist, starts, stepsize = fasthist.hist16bit(data, bins=None, step=1, use_numba=True)
+            
+        elif self.dtype in ['int32', 'uint32', 'int64', 'uint64', 'float16', 'float32', 'float64']:
+            hist, starts, stepsize = fasthist.histfloat(data, bins=65536, step=None, pow2snap=False, use_numba=True)
+            
+        self._cache['hist'] = hist
+        self._cache['starts'] = starts
+        self._cache['stepsize'] = stepsize    
+    
+    
+    def step_for_bins(self, bins):
+        if self.dtype in ['float16', 'float32', 'float64']:
+            return math.ceil(65536 / bins) 
+            
+        if len(self._cache.keys()) == 0:
+            self.calc_histogram()
+            
+        hist1 = self._cache['hist']  
+        
+        return math.ceil(len(hist1) / bins)  
+        
+    
+    @property    
+    def bins(self):
+        return len(self.starts())
+        
+        
+    def stepsize(self, step):
+        return self._cache['stepsize'] * step
+        
+
+    def sum(self):
+        return (self.histogram() * self.starts()).sum()
+        
+        
+    def mean(self):
+        n = self.n()
+        if n == 0:
+            return np.nan
+        return self.sum() / n
+
+    def sumsq(self):
+        return (self.histogram() * self.starts()**2).sum()
+        
+        
+    def min(self):
+        hist = self.histogram()
+        starts = self._cache['starts']
+        if len(starts) > 0:
+            return starts[0]
+        else:
+            return np.nan
+            
+        
+    def max(self):
+        hist = self.histogram()
+        starts = self._cache['starts']
+        if len(starts) > 0:
+            return starts[-1]
+        else:
+            return np.nan
+            
+        
+    def std(self):
+        n = self.n()
+        
+        if n >= 2:
+            result = ((self.sumsq() - ((self.sum() * 1.0) ** 2) / n) / (n - 1))
+            if result >= 0:
+                return result ** 0.5
+            else:
+                return np.nan
+        else:
+            return np.nan
         
         
 def apply_roi_slice(large_slices, roi_slices):
